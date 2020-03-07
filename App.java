623c06ddf088593491388206ce1292fafc50fc8d
Angelina James <scrog58@hotmail.com>
2020-03-06 18:36:21 -0700

    scanner

--- App.java
+++ App.java
@@ -1,12 +1,59 @@
 
+import java.util.Scanner;
+
 public class App {
 
 	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		
+		int numOfPlayers = 0;
+		String getPlayerOneName = "";
+		String getPlayerTwoName = "";
+		
 		Deck deck = new Deck();
 		deck.shuffle();
 		
-		Player playerOne = new Player("Ezra");
-		Player playerTwo = new Player("Clementine");
+		
+		System.out.println("1) One Player");
+		System.out.println("2) Two Players");
+		System.out.println("3) Play");
+		System.out.println("Welcome to the card game War!");
+		System.out.print("Please Select Number Players: ");
+		
+		numOfPlayers = scanner.nextInt();
+		
+		System.out.println("You selected: " + numOfPlayers);
+		
+		while(numOfPlayers != 3) {
+		if(numOfPlayers == 1) {
+			System.out.print("Name of Player One: ");
+			getPlayerOneName = scanner.nextLine();
+			
+			System.out.println("Welcome: " + getPlayerOneName);
+			
+			getPlayerTwoName = "Com";
+			
+		} else if(numOfPlayers == 2) {
+			System.out.print("Name of Player One: ");
+			getPlayerOneName = scanner.nextLine();
+			
+			System.out.println("Welcome: " + getPlayerOneName);
+			
+			System.out.println("Name of Player Two: ");
+			getPlayerTwoName = scanner.nextLine();
+			System.out.println("Welcome: " + getPlayerTwoName);
+			
+			
+		} else {
+			System.out.println("You didn't input how many players going to default names.");
+			getPlayerOneName = "Com1";
+			getPlayerTwoName = "Com2" ;
+		}
+	}
+		
+		
+		Player playerOne = new Player(getPlayerOneName);
+		Player playerTwo = new Player(getPlayerTwoName);
 		
 		
 		//pull every other number for player two, other card for player one
